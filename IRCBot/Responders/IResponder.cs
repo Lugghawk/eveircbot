78c2e16b60bfffcad82c176fbbffa4f2792003ff
SJShaw <fantastication@gmail.com>
2013-08-20 21:54:18 +1000

    basic dll loading

--- IRCBot/Responders/IResponder.cs
+++ IRCBot/Responders/IResponder.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace IRCBot.Responders
 {
@@ -22,6 +23,26 @@ namespace IRCBot.Responders
         /// <param name="connection">The stream which the response will be given on. Use Streamwriter.WriteLine() to send a line to IRC.</param>
         /// <param name="message">The input which to respond against.</param>
         void respond(IrcConnection connection, Input input);
+
+    }
+
+    public static class ResponderLoader {
+
+        public static IResponder loadCompiled(string filename, string classname) {
+            Assembly file = Assembly.LoadFile(filename);
+            foreach (Type type in file.GetTypes()) {
+                if (type.GetInterface("IResponder") != null && type.Name.Equals(classname)) {
+                    return Activator.CreateInstance(type) as IResponder;
+                }
+            }
+            return null;
+        }
+
+        public static IResponder loadSource(string filename) {
+
+
+            return null;
+        }
 
     }
 }
