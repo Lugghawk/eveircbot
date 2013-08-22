c75a1fb94be9120a63db4e5d82614a68960dad71
SJShaw <fantastication@gmail.com>
2013-08-23 01:55:12 +1000

    polishing pricechecks

--- IRCBot/Responders/Impl/PriceCheckResponder.cs
+++ IRCBot/Responders/Impl/PriceCheckResponder.cs
@@ -23,44 +23,46 @@ namespace IRCBot.Responders.Impl
         void IResponder.respond(IrcConnection connection, Input input)
         {
 
-            string [] message = input.message.Split(' ');
-            //string itemName = message[1];
-            string itemName = "";
-            for (int i = 1; i < message.Length; i++)
-            {
-                itemName += message[i];
-                if (i < message.Length-1)
-                {
-                    itemName += " ";
-                }
-
+            string [] message = input.message.Split(new char[] {' '}, 2);
+            if (message.Length < 2) {
+                connection.privmsg(input.target, "More than you can afford");
+                return;
             }
+            string itemName = message[1];
             if (itemName.Length < 3)
             {
                 connection.privmsg(input.target, "Please supply at least three characters");
             }
-            itemName += "%";//For like statement.
             
             
-            List<InvType> types = (List<InvType>)IrcBot.mySession.CreateCriteria<InvType>().Add(Restrictions.InsensitiveLike("typeName", itemName)).List<InvType>();
-            if (types == null || types.Count == 0)
+            List<InvType> types = (List<InvType>)IrcBot.mySession.CreateCriteria<InvType>().Add(Restrictions.InsensitiveLike("typeName", itemName+"%")).List<InvType>();
+            var names = from type in types select type.typeName;
+            if (types.Count == 1 || names.Contains(itemName))
+            {
+                foreach (InvType type in types) {
+                    if (type.typeName.ToLower() == itemName.ToLower()) {
+                        connection.privmsg(input.target, "Price of " + type.typeName + " is " + string.Format("{0:n}", getMarketPrice(type.typeID)) + " on average.");
+                        break;
+                    }
+                }
+
+            }
+            else if (types == null || types.Count == 0)
             {
                 connection.privmsg(input.target, "Can't find that item. Check spelling");
                 return;
             }
             else if (types.Count > 1 && types.Count <=5)
             {
-                string[] itemString  = new string[5];
+                string[] itemString  = new string[types.Count];
                 for (int i=0; i < types.Count; i++){
                     //If there is more than 1 result, concatenate them into a list and return to give an example.                 
                     itemString[i] = types.ElementAt(i).typeName;
                 }
                 connection.privmsg(input.target,"Found multiple Results: " + string.Join(", ",itemString) +".");
                 return;
-            }else if (types.Count == 1)
-            {   
-                InvType type = types.ElementAt(0);
-                connection.privmsg(input.target, "Price of "+ type.typeName + " is " + string.Format("{0:n}",getMarketPrice(type.typeID)) + " on average.");
+            } else if (types.Count > 5) {
+                connection.privmsg(input.target, String.Format("I found {0} matches for that, mind making it more specific?", types.Count));
             }
             
         }
